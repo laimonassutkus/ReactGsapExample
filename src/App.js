afd923666c3c548912617067258a0e9095ba7731
Laimonas Sutkus <laimonas.sutkus@gmail.com>
2024-03-27 12:18:16 +0200

    Gsap example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { gsap } from "gsap";
+import { useLayoutEffect, useRef } from 'react';
+
 
 function App() {
+  // Create a reference object that will be used for an html (usually a div element). 
+  // Use it like this: <div ref={comp}>. This way you say that we are going to target/modify 
+  // elements that are only wihtin that div.  
+  const comp = useRef(null)
+
+  // This is where we are going to apply all animations. 
+  // It all starts by calling "useLayoutEffect" function.
+  useLayoutEffect(() => {
+    let gsap_context = gsap.context(() => {
+      // All animations are happening in a timeline, hence we need to create one.
+      const timeline = gsap.timeline({ repeat: -1 })
+
+      // Start targeting elements and animating them. 
+      timeline.to("#App-logo", {
+        opacity: 0,
+        duration: 2,
+        delay: 2
+      }).to("#App-logo", {
+        opacity: 1,
+        duration: 2
+      })
+    }, comp)
+
+    return () => gsap_context.revert()
+  }, [])
+
   return (
-    <div className="App">
+    // Here we reference that "useRef" object.
+    <div className="App" ref={comp}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} id="App-logo" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
